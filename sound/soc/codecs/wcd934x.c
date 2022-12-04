0540a2d3147d0e15c0eff3b9e91f4ebda1172e60
Joel Selvaraj <joelselvaraj.oss@gmail.com>
2022-12-04 16:34:43 -0600

    ASoC: codecs: wcd934x: fix wcd->micb2_mv being read before assigned

--- sound/soc/codecs/wcd934x.c
+++ sound/soc/codecs/wcd934x.c
@@ -2950,6 +2950,7 @@ static int wcd934x_mbhc_init(struct snd_soc_component *component)
 {
 	struct wcd934x_ddata *data = dev_get_drvdata(component->dev->parent);
 	struct wcd934x_codec *wcd = snd_soc_component_get_drvdata(component);
+	struct wcd_mbhc_config *cfg = &wcd->mbhc_cfg;
 	struct wcd_mbhc_intr *intr_ids = &wcd->intr_ids;
 
 	intr_ids->mbhc_sw_intr = regmap_irq_get_virq(data->irq_data,
@@ -2972,6 +2973,8 @@ static int wcd934x_mbhc_init(struct snd_soc_component *component)
 		wcd->mbhc = NULL;
 		return -EINVAL;
 	}
+
+	cfg->micb_mv = wcd->common.micb_mv[1];
 
 	snd_soc_add_component_controls(component, impedance_detect_controls,
 				       ARRAY_SIZE(impedance_detect_controls));
@@ -5832,7 +5835,6 @@ static int wcd934x_codec_parse_data(struct wcd934x_codec *wcd)
 	cfg->anc_micbias = MIC_BIAS_2;
 	cfg->v_hs_max = WCD_MBHC_HS_V_MAX;
 	cfg->num_btn = WCD934X_MBHC_MAX_BUTTONS;
-	cfg->micb_mv = wcd->common.micb_mv[1];
 	cfg->linein_th = 5000;
 	cfg->hs_thr = 1700;
 	cfg->hph_thr = 50;
