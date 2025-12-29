a1659c647eecc2b5a809461172f9b0fa2e5d3cda
Joel Selvaraj <foss@joelselvaraj.com>
2025-12-29 02:03:34 -0600

    Revert "ASoC: codecs: wcdxxxx: use sdw_slave_get_current_bank helper"
    This reverts commit 45a3295a3005f7782054a153312ba81d28eb7664.

--- sound/soc/codecs/wcd938x.c
+++ sound/soc/codecs/wcd938x.c
@@ -1094,7 +1094,8 @@ static int wcd938x_tx_swr_ctrl(struct snd_soc_dapm_widget *w,
 	int bank;
 	int rate;
 
-	bank = sdw_slave_get_current_bank(wcd938x->sdw_priv[AIF1_CAP]->sdev);
+	bank = (wcd938x_swr_get_current_bank(wcd938x->sdw_priv[AIF1_CAP]->sdev)) ? 0 : 1;
+	bank = bank ? 0 : 1;
 
 	switch (event) {
 	case SND_SOC_DAPM_PRE_PMU:
